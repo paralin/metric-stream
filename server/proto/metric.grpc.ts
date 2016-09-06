e181c517041e68266bbd11da3e7b561c00d0c183
Christian Stewart <christian@paral.in>
2016-09-06 12:17:45 -0400

    fix(grpc): fix clazz null errors

--- server/proto/metric.grpc.ts
+++ server/proto/metric.grpc.ts
@@ -1,5 +1,7 @@
 import { builder } from '../../proto/builder';
 import * as grpc from 'grpc';
 
+// Build the metric namespace
+builder.build('metric');
 export const GRPCMetricServiceProto =
   grpc.loadObject(builder.lookup('metric.MetricService'));
