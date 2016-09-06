e5e71f63f54c9d037e478d8347f903ab1d2e1a1b
Christian Stewart <christian@paral.in>
2016-09-06 10:58:57 -0400

    chore(typings): ignore typings dirs

--- typings/index.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="globals/bytebuffer/index.d.ts" />
-/// <reference path="globals/long/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
-/// <reference path="globals/protobufjs/index.d.ts" />
-/// <reference path="modules/lodash/index.d.ts" />
