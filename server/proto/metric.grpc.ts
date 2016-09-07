94d53c550fef2b7f1b48aaceefc84eb7f81f7f0d
Christian Stewart <christian@paral.in>
2016-09-07 14:42:44 -0400

    refactor(server): remove old grpc builder files

--- server/proto/metric.grpc.ts
+++
@@ -1,7 +0,0 @@
-import { builder } from '../../proto/builder';
-import * as grpc from 'grpc';
-
-// Build the metric namespace
-builder.build('metric');
-export const GRPCMetricServiceProto =
-  grpc.loadObject(builder.lookup('metric.MetricService'));
